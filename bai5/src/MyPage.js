10608040e096114ad617d638b64fd750875a4518
NgoDacLoi <91691659+NgoDacLoi@users.noreply.github.com>
2022-11-03 15:31:39 +0700

    bai5

---
+++ bai5/src/MyPage.js
@@ -0,0 +1,25 @@
+
+import * as React from "react";
+
+const MyFeature= React.lazy(()=>
+Promise.all([
+   import("./MyFeature"),
+   new Promise((resolve)=>{
+      setTimeout(() => {
+         resolve();
+      }, 3000);
+   })
+]).then(([m])=>m)
+);
+
+function MyPage(){
+   return (
+      <>
+      <h1>MyPage</h1> 
+      <MyFeature/>
+         </>
+   );
+}
+export default MyPage;
+
+ 
