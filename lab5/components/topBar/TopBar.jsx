ea3dcb86eec8e1ab526e51a076eb5ecdfa88904f
NgoDacLoi <91691659+NgoDacLoi@users.noreply.github.com>
2022-11-17 12:50:35 +0700

    lab5

---
+++ lab5/components/topBar/TopBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  AppBar, Toolbar, Typography
+} from '@material-ui/core';
+import './TopBar.css';
+import fetchModel from '../../lib/fetchModelData';
+
+
+/**
+ * Define TopBar, a React componment of UIT project #5
+ */
+class TopBar extends React.Component {
+  constructor(props) {
+    super(props);
+    
+  }
+  
+
+  render() {
+   
+    return (
+      <AppBar className="UIT-topbar-appBar" position="absolute">
+        <Toolbar>
+          <Typography variant="h5" color="inherit">
+                NGO DAC LOI
+          </Typography>
+
+          <Typography className='right' variant="h5" color="inherit">
+          {this.props.view === 'photo' ? 'Photos of ' : ''}
+          </Typography>
+
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+export default TopBar;
